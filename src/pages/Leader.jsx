abfd04ec18470eb3cefff37a68a2d74b6cee37e8
Pathfinder1613 <quin.strickland@outlook.com>
2026-06-12 01:53:51 -0500

    the chaos begins lol 67 lol

--- src/pages/Leader.jsx
+++ src/pages/Leader.jsx
@@ -1,63 +1,42 @@
 import { useState } from 'react'
-import LeaderData from '../services/Leaders.json'
+import LeaderItem from './LeaderItem';
+
+
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 // css
 import '../Styles/LeaderPage.css';
 
-
-// imgs
-import UDFICON from '../assets/UDFPlacholdericon.svg';
-import SakupenICON from '../assets/SakupenPlaceHolder.svg';
-import StormICON from '../assets/StromPlaceHolder.svg';
-import BogIcon from '../assets/TheBogPlaceHolder.svg';
-import TrogsIcon from '../assets/TheRoglerIcon.svg'
-
-
-const FACTIONS = [
-    {
-        name: "UDF",
-        icon: UDFICON,
-    },
-    {
-        name: "Sakupen",
-        icon: SakupenICON,
-    },
-    {
-        name: "The Storm",
-        icon: StormICON,
-    },
-    {
-        name: "placeholder",
-        icon: BogIcon,
-    },
-    {
-        name: "Trogs",
-        icon: TrogsIcon
-    }
-]
-
 export default function LeaderPage() {
-    const [leaders] = useState(LeaderData.Leader)
 
     return (
         <>
-            {FACTIONS.map((faction) => (
-                <div key={faction} className={`Leader-section-${faction.name.replace(" ", "-")}`}>
-                    <h1>{faction.name}</h1>
-                    <div>
-                        <img src={faction.icon} />
-                        {leaders
-                            .filter((leader) => leader.faction === faction.name)
-                            .map((leader) => (
-                                <li key={leader.id}>
-                                    <h2>★ {leader.name}</h2>
-                                    <p>{leader.leaderType} | {leader.treeType}</p>
-                                    <p>{leader.subHeader}</p>
-                                </li>
-                            ))}
-                    </div>
-                </div>
-            ))}
+
+            <Swiper
+                // install Swiper modules
+                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                spaceBetween={50}
+                slidesPerView={3}
+                navigation
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log('slide change')}
+            >
+                <SwiperSlide><LeaderItem factionName="UDF"/></SwiperSlide>
+                <SwiperSlide><LeaderItem factionName="Sakupen"/></SwiperSlide>
+                <SwiperSlide><LeaderItem factionName="The Storm"/></SwiperSlide>
+                <SwiperSlide><LeaderItem factionName="placeholder"/></SwiperSlide>
+                <SwiperSlide><LeaderItem factionName="Trogs"/></SwiperSlide>
+                ...
+            </Swiper>
+
 
 
 
