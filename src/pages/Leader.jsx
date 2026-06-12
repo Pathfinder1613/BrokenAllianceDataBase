01f8713da040b2d1f29c5f08f9f9c76e16cbfe55
SlugToast <116036080+SlugToast@users.noreply.github.com>
2026-06-12 05:43:20 -0500

    reformated the page, some other changes here and there

--- src/pages/Leader.jsx
+++ src/pages/Leader.jsx
@@ -13,11 +13,14 @@ import 'swiper/css/scrollbar';
 // css
 import '../Styles/LeaderPage.css';
 
+import FACTIONS from '../services/Factions.json';
+import LEADERS from '../services/Leaders.json'
+
 export default function LeaderPage() {
+    // NOTE: When referencing images dynamically, images MUST be put in the 'public' folder.
 
     return (
         <>
-
             <Swiper
                 // install Swiper modules
                 modules={[Navigation, Pagination, Scrollbar, A11y]}
@@ -25,14 +28,35 @@ export default function LeaderPage() {
                 slidesPerView={1}
                 navigation
                 pagination={{ clickable: true }}
-                scrollbar={{ draggable: true }}
+                scrollbar={false}
                 onSwiper={(swiper) => console.log(swiper)}
                 onSlideChange={() => console.log('slide change')}
             >
-                <SwiperSlide><LeaderItem factionName="UDF"/></SwiperSlide>
-                <SwiperSlide><LeaderItem factionName="Sakupen"/></SwiperSlide>
-                <SwiperSlide><LeaderItem factionName="The Storm"/></SwiperSlide>
-                <SwiperSlide><LeaderItem factionName="Trogs"/></SwiperSlide>
+                <div>
+                    {FACTIONS.map((faction) => {
+                        const leaders = LEADERS.filter((leader) => leader.faction === faction.name);
+
+                        return (
+                            <SwiperSlide key={`faction-slide-${faction.name}`}>
+                                <div style={{marginBottom: '86px'}}>
+                                    <img style={{ width: '82px', height: '82px', float: 'left', marginRight: '16px' }} src={`../../images/${faction.name.replace(" ", "")}_Icon.svg`} />
+                                    <h1 style={{ float: "left", color: faction.color }}>{faction.name}</h1>
+                                </div>
+                                <div className="leader-list">
+                                    {leaders.map((leader) => (
+                                        <div className="leader-container" key={leader.id}>
+                                            <span style={{fontSize: '1.5em', color: faction.color, fontWeight: 'bold'}}>★ {leader.name}</span>
+                                            <br/>
+                                            <span style={{fontWeight: 'bold'}}>{leader.leaderType}</span>
+                                            <br/>
+                                            <span style={{fontSize: '1.1em', fontStyle: 'italic', opacity: 0.75}}>"{leader.quote}"</span>
+                                        </div>
+                                    ))}
+                                </div>
+                            </SwiperSlide>
+                        )
+                    })}
+                </div>
             </Swiper>
 
 
--- src/pages/LeaderItem.jsx
+++ src/pages/LeaderItem.jsx
@@ -1,36 +1,10 @@
 import LeaderData from '../services/Leaders.json'
-import { useState } from 'react';
 
-// imgs
-import UDFICON from '../assets/UDFPlacholdericon.svg';
-import SakupenICON from '../assets/SakupenPlaceHolder.svg';
-import StormICON from '../assets/StromPlaceHolder.svg';
-import BogIcon from '../assets/TheBogPlaceHolder.svg';
-import TrogsIcon from '../assets/TheRoglerIcon.svg'
+import FACTIONS from '../services/Factions.json'
 
-
-export default function LeaderItem( props ) {
+export default function LeaderItem(props) {
     const leaders = LeaderData.Leader
 
-
-    const FACTIONS = [
-        {
-            name: "UDF",
-            icon: UDFICON,
-        },
-        {
-            name: "Sakupen",
-            icon: SakupenICON,
-        },
-        {
-            name: "The Storm",
-            icon: StormICON,
-        },
-        {
-            name: "Trogs",
-            icon: TrogsIcon
-        }
-    ]
 
     // const faction = FACTIONS.find((f) => f.name === factionName);
 
@@ -41,20 +15,20 @@ export default function LeaderItem( props ) {
                     <div key={faction.name} className={`Leader-section-${faction.name.replace(" ", "-")}`}>
                         <h1>{faction.name}</h1>
                         <div>
-                            <img src={faction.icon} alt={faction.name}/>
+                            <img src={`../../images/${faction.name.replace(" ", "")}_Icon.svg`}/>
                             <ul>
                                 {leaders
-                                .filter((leader) => leader.faction === faction.name)
-                                .map((leader) => (
-                                    <li key={leader.id}>
-                                        <h2>★ {leader.name}</h2>
-                                        <p>{leader.leaderType} | {leader.treeType}</p>
-                                        <p>{leader.subHeader}</p>
-                                    </li>
-                                ))}
+                                    .filter((leader) => leader.faction === faction.name)
+                                    .map((leader) => (
+                                        <li key={leader.id}>
+                                            <h2>★ {leader.name}</h2>
+                                            <p>{leader.leaderType} | {leader.treeType}</p>
+                                            <p>{leader.subHeader}</p>
+                                        </li>
+                                    ))}
 
                             </ul>
-                            
+
                         </div>
                     </div>
                 ))}
