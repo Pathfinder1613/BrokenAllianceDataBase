5b74977ed12fbc6bb987aa0e659c6580a9f54fb5
SlugToast <116036080+SlugToast@users.noreply.github.com>
2026-06-16 18:04:38 -0500

    changed AIO page structure to be more like unitdb.faforever.com

--- src/pages/AllInOne.jsx
+++ src/pages/AllInOne.jsx
@@ -1,6 +1,6 @@
 import '../Styles/AIO.css'
 
-import LEADERS from '../services/NewTestData/Leader.json'
+import LEADERS from '../services/Leaders.json'
 import UNITS from '../services/Units.json'
 
 // import FACTION from '../services/Factions.json' should we update that json?
@@ -9,48 +9,92 @@ const FACTIONS = [
         id: "udf",
         name: "UDF",
         color: "#3535c0",
+        leaders: [
+            "president_enhet",
+            "commander_hawk",
+            "iq_iris_quantum",
+        ]
     },
     {
         id: "sakupen",
         name: "Sakupen",
         color: "#661414",
+        leaders: [
+            "kizer",
+            "e_11",
+            "3c_commander_class_chassis",
+        ],
     },
     {
         id: "the_storm",
         name: "The Storm",
         color: "#531d8a",
+        leaders: [
+            "amalgam",
+            "phantoms"
+        ]
     },
     {
         id: "the_trogs",
         name: "Trogs",
-        color: "#3ca03c"
+        color: "#3ca03c",
+        leaders: [
+
+        ]
     },
 ]
+
+function CreateLeaderRows() {
+    const content = [];
+    const empty = (<div className="aio-row-item"></div>)
+
+    // finding the faction with the most amount of leaders
+    let max_leaders = 0;
+    FACTIONS.forEach((element) => {
+        max_leaders = Math.max(max_leaders, element.leaders.length)
+    })
+    
+    for (let i = 0; i < max_leaders; i++) {
+        content.push((
+            <div className="aio-row">
+                {FACTIONS.map((faction, ii) => {
+                    const faction_id = faction.leaders[i];
+                    if (!faction_id) return empty;
+
+                    const leader = LEADERS[faction_id];
+                    if (!leader) return empty;
+
+                    return (
+                        <div className="aio-row-item aio-selectable">
+                            <span className="aio-leader-name">{leader.name}</span>
+                            <span className="aio-leader-type">{leader.type}</span>
+                            <span className="aio-leader-quote">"{leader.quote}"</span>
+                        </div>
+                    )
+                })}
+            </div>
+        ))
+    }
+
+    return content;
+}
 
 export default function AllInOne() {
     return (
         <div className="aio-main">
-            {FACTIONS.map((faction) => (
-                <div key={faction.id} className="aio-faction-container">
-                    <div style={{ backgroundColor: faction.color }} className="aio-faction-header">
-                        <span className="aio-faction-name">{faction.name}</span>
-                        <div style={{ maskImage: `url(/images/icons/${faction.id}.svg)` }} className="aio-faction-mini-icon" />
-                    </div>
-                    {LEADERS.Leader.filter((leader) => leader.faction.toLowerCase() === faction.id).map((leader) =>
-                    (<div key={leader.id} className="aio-leader-panel">
-                        <div className="aio-leader-name">{leader.name}</div>
-                        <div className="aio-leader-type">{leader.title}</div>
-                        <div className="aio-leader-quote">"{leader.tagline}"</div>
-                    </div>))}
-                    {UNITS.filter((unit) => unit.faction === faction.id).map((unit) => (
-                        <div key={unit.id} className="aio-unit-panel">
-                            <span>T{unit.tier}</span>
-                            <span> {unit.type}</span>
-                            <span className="aio-unit-name"> {unit.name ? `"${unit.name}"` : ""}</span>
+            <>
+                <div className="aio-faction-header-container">
+                    {FACTIONS.map((faction) => (
+                        <div style={{ background: faction.color }} className="aio-faction-header">
+                            <span>{faction.name}</span>
+                            <div style={{ maskImage: `url(/images/icons/${faction.id}.svg)`}} className="aio-faction-icon"></div>
                         </div>
                     ))}
                 </div>
-            ))}
+                <div className="aio-row-container">
+                    {CreateLeaderRows()}
+                </div>
+            </>
         </div>
     )
 }
